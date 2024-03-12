8e64bb4fb4d9e6eee889287583fc175736f4b26c
yanwuzhang <yanwuzhang@tencent.com>
2024-03-12 16:19:51 +0800

    新增AbilitySystemComponent

--- Source/GASProject/GASProject.Build.cs
+++ Source/GASProject/GASProject.Build.cs
@@ -8,6 +8,10 @@ public class GASProject : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput",
+			"GameplayAbilities", "GameplayTags", "GameplayTasks"
+		});
 	}
 }
